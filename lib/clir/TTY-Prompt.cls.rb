e7dbe1398d922d4272d442430e261cad49631c26
PhilippePerret <philippe.perret@yahoo.fr>
2022-09-16 07:55:11 +0200

    Poursuite tests sur TTY

--- lib/clir/TTY-Prompt.cls.rb
+++ lib/clir/TTY-Prompt.cls.rb
@@ -45,18 +45,6 @@ module TTY
       end
     end
 
-    ##
-    # In test mode, return fake-user inputs (keyboard inputs)
-    def inputs
-      @inputs ||= begin
-        if ENV['CLI_TEST_INPUTS']
-          JSON.parse ENV['CLI_TEST_INPUTS']
-        else
-          []
-        end
-      end
-    end
-
   end #/class MyPrompt
 end
 
@@ -68,15 +56,174 @@ end
 # defined in CLITests for user interaction.
 # 
 module TestTTYMethods
+
+  ##
+  # Error class raised when there's no more input values to
+  # bring back.
+  # 
   class CLiTestNoMoreValuesError < StandardError; end
 
-  def ask(*)
-    puts "Je suis le ask utilisé en mode test"
+  ##
+  # In test mode, return fake-user inputs (keyboard inputs)
+  def inputs
+    @inputs ||= begin
+      if ENV['CLI_TEST_INPUTS']
+        JSON.parse ENV['CLI_TEST_INPUTS']
+      else
+        []
+      end
+    end
+  end
+
+  ##
+  # @return next fake-user input or raise a error if no more
+  # value to return.
+  # 
+  def next_input
+    if inputs.any?
+      inputs.shift
+    else
+      raise CLiTestNoMoreValuesError
+    end
+  end
+
+  def ask(*args, &block)
+    Responder.new(self, 'ask', *args, &block).response
   end
   def select(*)
     puts "Je suis le select utilisé en mode test"
   end
-end
+
+
+  # --- Class TestTTYMethods::Responder ---
+
+  class Responder
+
+    attr_reader :prompt, :type, :args
+    
+    ##
+    # The input for this responder
+    attr_reader :input
+    
+    def initialize(prompt, type, *args, &block)
+      @prompt = prompt
+      @type   = type
+      @args   = args
+      instance_eval(&block) if block_given?
+    end
+
+    ##
+    # Main method to evaluate the respond to give
+    # (the respond that user would have given)
+    #
+    # @return the fake-user input transformed (for example, if no
+    # value has been given, return the default value)
+    def response
+      @input  = prompt.next_input
+      self.send(tty_method)
+      return input
+    end
+
+    # --- Twin TTY::Prompt methods ---
+    # --- They treat input value as needed ---
+
+    def __ask
+      if input.is_a?(String) && input.downcase == 'default'
+        @input = default_value
+      end
+    end
+
+    # @return self Twin TTY method
+    # p.e. '__ask' ou '__select'
+    def tty_method
+      @tty_method ||= "__#{type}".to_sym
+    end
+
+    # --- Default value ---
+
+    ##
+    # @return the default value
+    def default_value
+      if defined?(@default)
+        return @default
+      elsif args[-1].is_a?(Hash)
+        args.last[:default]
+      else
+        nil
+      end
+    end
+
+    # --- Les méthodes communes qui permettent de définir
+    #     le répondeur ---
+
+    ##
+    # To define and get select choices
+    # 
+    def choices(vals = nil)
+      if vals.nil?
+        return @choices
+      else
+        @choices ||= []
+        @choices += vals
+      end
+    end
+
+    ##
+    # For select or multi-select, to add a choice
+    # 
+    def choice menu, value = nil, options = nil
+      @choices ||= []
+      @choices << {name:menu, value:value||menu, options:options}
+    end
+
+    ##
+    # To define the number of items displayed with a
+    # select or multiselect
+    # 
+    def per_page(*args)
+      # Rien à faire ici
+    end
+
+    ##
+    # To define the default value
+    # 
+    def default(value)
+      @default = value
+    end
+
+    ##
+    # For real TTYPrompt compatibility
+    def enum(*args)
+      # Ne rien faire
+    end
+
+    ##
+    # For real TTYPrompt compatibility
+    def help(str)
+      # Ne rien faire
+    end
+
+    ##
+    # For real TTYPrompt compatibility
+    def validate(*arg, &block)
+      # Ne rien faire
+      # TODO Plus tard on pourra vérifier les validations aussi
+    end
+
+    ##
+    # For real TTYPrompt compatibility
+    def convert(*arg, &block)
+      # Ne rien faire
+      if block_given?
+        # TODO Plus tard on pourra vérifier les conversions aussi
+        # Mais attention : c'est pas forcément donné par block
+      end
+    end
+
+
+  end #/class Responder
+
+end #/module TestTTYMethods
 
 Q = TTY::MyPrompt.new(symbols: {radio_on:"☒", radio_off:"☐"})
 
--- test/tty_prompt_test.rb
+++ test/tty_prompt_test.rb
@@ -41,12 +41,108 @@ class TTYPromptTests < Minitest::Test
     ENV['CLI_TEST'] = 'true'
     ENV['CLI_TEST_INPUTS'] = ["Marion Mic"].to_json
     CLI.init
-    res = nil
-    out, err = capture_io { res = Q.ask("What's your name?") }
-    assert_match out, "What's your name\?"
-    assert_match out, "Marion"
+    res = Q.ask("What's your name?")
+    assert_match "Marion", res
     assert res == 'Marion Mic'
-    assert_empty err
   end
+
+
+  # --- Test du Responder ---
+
+  def rew_tty_responder(type, question = 'The question?')
+    @tty_responder = nil
+    tty_responder(type, question)
+  end
+
+  def tty_responder(type = 'ask', question = 'What’s your name?')
+    @tty_responder ||= begin
+      CLI.init
+      TestTTYMethods::Responder.new(Q, type, question)
+    end
+  end
+
+  def test_Responder_class_exists
+    assert defined?(TestTTYMethods::Responder)
+  end
+
+  def test_initialisation_Responder
+    r = tty_responder
+    assert_equal r.class.name, 'TestTTYMethods::Responder'
+  end
+
+  def test_responder_respond_to_usual_methods
+    r = tty_responder
+    # - choices -
+    assert r.respond_to? :choices
+    leschoix = [{name:'Premier', value:1}, {name:'Deuxième', value:2}]
+    r.choices(leschoix)
+    assert_equal r.choices, leschoix
+    # -choice -
+    assert r.respond_to? :choice
+    r.choice "Troisième", 3
+    r.choice "Quatrième", 4
+    thechoix = r.choices
+    assert_equal 4, thechoix.count
+    assert_equal 'Quatrième', thechoix[3][:name]
+    assert_equal 4, thechoix[3][:value]
+    # - per_page - (mais ne doit rien faire)
+    assert r.respond_to? :per_page
+    # - default - 
+    assert r.respond_to? :default
+    r.default "Valeur par défaut"
+    assert_equal 'Valeur par défaut', r.instance_variable_get('@default')
+    # - enum - (mais rien à faire)
+    assert r.respond_to? :enum
+    # - help - (mais rien à faire)
+    assert r.respond_to? :help
+    # - validate - (mais rien à faire pour le moment)
+    assert r.respond_to? :validate
+    # - convert - (mais rien à faire)
+    assert r.respond_to? :convert
+    r.convert do |c|
+      # On peut donner la valeur par block
+      c.to_i
+    end
+  end
+
+  ##
+  # Pour vérifier que Q.next_input retourne bien la prochaine
+  # valeur entrée ou génère une erreur.
+  def test_tty_next_input_method
+    ENV['CLI_TEST'] = 'true'
+    ENV['CLI_TEST_INPUTS'] = nil
+    CLI.init
+    assert_respond_to Q, :next_input
+    # Raise if no more value for input
+    assert_raises { Q.next_input }
+    # Dont raise if more value
+    ENV['CLI_TEST_INPUTS'] = ['A', 'B', 'C'].to_json
+    CLI.init
+    assert_equal 'A', Q.next_input
+    assert_equal 'B', Q.next_input
+    assert_equal 'C', Q.next_input
+    assert_raises { Q.next_input }
+  end
+
+  ##
+  # Pour vérifier que la méthode Responder#evaluate, qui 
+  # retourne la valeur qu'aurait renvoyé l'utilisateur, fonctionne
+  # conformément aux attentes, quel que soit le type de demande (ask,
+  # select ou autre).
+  #
+  def test_tty_responder_response
+    r = tty_responder
+    assert r.respond_to? :response
+  end
+
+  def test_tty_responder_response_with_ask
+    ENV['CLI_TEST'] = 'true'
+    r = rew_tty_responder('ask')
+    ENV['CLI_TEST_INPUTS'] = ["Marion MIC", 'default'].to_json
+    assert_equal "Marion MIC", r.response
+    r.default("MIC Marion")
+    assert_equal 'MIC Marion', r.response
+  end
+
 
 end
