e086ded82c119e614c5096a44caaa97d61bf6ac8
PhilippePerret <philippe.perret@yahoo.fr>
2022-12-14 14:49:27 +0100

    Ajout de constantes date

--- lib/clir/Date_utils.rb
+++ lib/clir/Date_utils.rb
@@ -7,6 +7,26 @@
 =end
 require 'date'
 
+MOIS = {
+  1 => {court: 'jan', long: 'janvier'},
+  2 => {court: 'fév', long: 'février'},
+  3 => {court: 'mars', long: 'mars'},
+  4 => {court: 'avr', long: 'avril'},
+  5 => {court: 'mai', long: 'mai'},
+  6 => {court: 'juin', long: 'juin'},
+  7 => {court: 'juil', long: 'juillet'},
+  8 => {court: 'aout', long: 'aout'},
+  9 => {court: 'sept', long: 'septembre'},
+  10 => {court: 'oct', long: 'octobre'},
+  11 => {court: 'nov', long: 'novembre'},
+  12 => {court: 'déc', long: 'décembre'}
+}
+
+DAYNAMES = [
+  'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'
+]
+
+
 # @return A date for a file, now
 # @example
 #   date_for_file # => "2022-12-14"
